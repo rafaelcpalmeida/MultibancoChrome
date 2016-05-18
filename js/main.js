854146a8fe3312aec6079c8b8601c30393b62e29
Rafael Almeida <rafaelcpalmeida@gmail.com>
2016-05-18 20:11:21 +0100

    Project refactoring. Version 1.3.2 is now OOP

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+    var mb = new Multibanco();
+    
     $("#tabs").tabs();
 
     $("#dataForm").dialog({
@@ -63,7 +65,7 @@ $(function () {
             if (entidade > 0 && subentidade > 0) {
                 if (valor > 0 && id > 0 && entidade > 0 && subentidade > 0) {
                     if (valor < 999999) {
-                        var ref = GetPaymentRef(entidade, subentidade, id, valor);
+                        var ref = mb.GetPaymentRef(entidade, subentidade, id, valor);
 
                         $("#entidadeGerar").val(ref["entidade"]);
                         $("#referenciaGerar").val(ref["referencia"]);
@@ -113,7 +115,7 @@ $(function () {
             var subentidade = referencia.substring(0, 3);
             var id = referencia.substring(3, 7);
 
-            var referenciaGerada = GetPaymentRef(entidade, subentidade, id, valor);
+            var referenciaGerada = mb.GetPaymentRef(entidade, subentidade, id, valor);
 
             if (referencia.replace(/\s/g, "") == referenciaGerada["referencia"].replace(/\s/g, "")) {
                 $("#messageVerificar").text("Referência válida").css("color", "#007F00");
@@ -140,86 +142,3 @@ $(function () {
     });
 
 });
-
-function GetPaymentRef(_ENTIDADE, _SUBENTIDADE, _ID, _VALOR) {
-
-    var ENT_CALC = (51 * parseInt(String(_ENTIDADE).charAt(0)) +
-        73 * parseInt(String(_ENTIDADE).charAt(1)) +
-        17 * parseInt(String(_ENTIDADE).charAt(2)) +
-        89 * parseInt(String(_ENTIDADE).charAt(3)) +
-        38 * parseInt(String(_ENTIDADE).charAt(4)));
-
-    var iCHECKDIGITS = 0;
-    var sTMP = "";
-
-    sTMP = String(Right("000" + _SUBENTIDADE.toString(), 3) + Right("0000" + _ID.toString(), 4) + Right("00000000" + (parseFloat(_VALOR) * 100).toFixed(0), 8));
-
-    //Calculate check digits
-    iCHECKDIGITS =
-        98 - (parseInt(ENT_CALC) +
-            3 * parseInt(sTMP.charAt(14)) +
-            30 * parseInt(sTMP.charAt(13)) +
-            9 * parseInt(sTMP.charAt(12)) +
-            90 * parseInt(sTMP.charAt(11)) +
-            27 * parseInt(sTMP.charAt(10)) +
-            76 * parseInt(sTMP.charAt(9)) +
-            81 * parseInt(sTMP.charAt(8)) +
-            34 * parseInt(sTMP.charAt(7)) +
-            49 * parseInt(sTMP.charAt(6)) +
-            5 * parseInt(sTMP.charAt(5)) +
-            50 * parseInt(sTMP.charAt(4)) +
-            15 * parseInt(sTMP.charAt(3)) +
-            53 * parseInt(sTMP.charAt(2)) +
-            45 * parseInt(sTMP.charAt(1)) +
-            62 * parseInt(sTMP.charAt(0))) % 97;
-
-    var _PaymentRef = Right("000" + _SUBENTIDADE, 3) + " " + Mid(Right("0000" + _ID, 4), 0, 3) + " " + Mid(Right("0000" + _ID, 4), 3, 1) + Right("00" + iCHECKDIGITS.toString(), 2);
-
-    return { "entidade": _ENTIDADE, "referencia": _PaymentRef, "valor": _VALOR };
-}
-
-//Mid Function
-function Mid(value, index, n) {
-    var result = String(value).substring(index, index + n);
-    return result;
-}
-//Right function
-function Right(value, n) {
-    var result = String(value).substring(String(value).length, String(value).length - n);
-    return result;
-}
-
-function formatAsMoney(mnt) {
-    mnt -= 0;
-    mnt = (Math.round(mnt * 100)) / 100;
-    return (mnt == Math.floor(mnt)) ? mnt + '.00' : ((mnt * 10 == Math.floor(mnt * 10)) ? mnt + '0' : mnt);
-}
-
-function StringFormatVerify(value) {
-    var count = 0;
-    var result = "";
-    for (var i = 0; i <= String(value).length; i++) {
-        if ((String(value).charAt(i) == ".") || (String(value).charAt(i) == ",")) count++;
-    }
-    if (count > 1) {
-
-        for (var i = 0; i <= String(value).length; i++) {
-            if (count > 1) {
-                if ((String(value).charAt(i) == ".") || (String(value).charAt(i) == ",")) {
-                    count--;
-                }
-                else {
-                    result += String(value).charAt(i);
-                }
-            }
-            else {
-                result += String(value).charAt(i);
-            }
-        }
-
-    }
-    else {
-        result = value;
-    }
-    return String(result).replace(",", ".");
-}
---
+++ js/multibanco.js
@@ -0,0 +1,49 @@
+var Multibanco = function() { };
+
+Multibanco.prototype.GetPaymentRef = function(_ENTIDADE, _SUBENTIDADE, _ID, _VALOR) {
+
+    var ENT_CALC = (51 * parseInt(String(_ENTIDADE).charAt(0)) +
+        73 * parseInt(String(_ENTIDADE).charAt(1)) +
+        17 * parseInt(String(_ENTIDADE).charAt(2)) +
+        89 * parseInt(String(_ENTIDADE).charAt(3)) +
+        38 * parseInt(String(_ENTIDADE).charAt(4)));
+
+    var iCHECKDIGITS = 0;
+    var sTMP = "";
+
+    sTMP = String(this.Right("000" + _SUBENTIDADE.toString(), 3) + this.Right("0000" + _ID.toString(), 4) + this.Right("00000000" + (parseFloat(_VALOR) * 100).toFixed(0), 8));
+
+    //Calculate check digits
+    iCHECKDIGITS =
+        98 - (parseInt(ENT_CALC) +
+            3 * parseInt(sTMP.charAt(14)) +
+            30 * parseInt(sTMP.charAt(13)) +
+            9 * parseInt(sTMP.charAt(12)) +
+            90 * parseInt(sTMP.charAt(11)) +
+            27 * parseInt(sTMP.charAt(10)) +
+            76 * parseInt(sTMP.charAt(9)) +
+            81 * parseInt(sTMP.charAt(8)) +
+            34 * parseInt(sTMP.charAt(7)) +
+            49 * parseInt(sTMP.charAt(6)) +
+            5 * parseInt(sTMP.charAt(5)) +
+            50 * parseInt(sTMP.charAt(4)) +
+            15 * parseInt(sTMP.charAt(3)) +
+            53 * parseInt(sTMP.charAt(2)) +
+            45 * parseInt(sTMP.charAt(1)) +
+            62 * parseInt(sTMP.charAt(0))) % 97;
+
+    var _PaymentRef = this.Right("000" + _SUBENTIDADE, 3) + " " + this.Mid(this.Right("0000" + _ID, 4), 0, 3) + " " + this.Mid(this.Right("0000" + _ID, 4), 3, 1) + this.Right("00" + iCHECKDIGITS.toString(), 2);
+
+    return { "entidade": _ENTIDADE, "referencia": _PaymentRef, "valor": _VALOR };
+}
+
+//Mid Function
+Multibanco.prototype.Mid = function(value, index, n) {
+    var result = String(value).substring(index, index + n);
+    return result;
+}
+//Right function
+Multibanco.prototype.Right = function(value, n) {
+    var result = String(value).substring(String(value).length, String(value).length - n);
+    return result;
+}
